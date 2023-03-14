28f30a9b7b80ef462f65a7f59e7e78c5975e8f9c
Esther Rose <estherrose046@gmail.com>
2023-03-14 16:42:19 -0500

    updated with dim background on menu open

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 const nav = document.querySelector(".mobile-nav");
 const menuitem = document.querySelector(".mobile-nav > li");
-
+const dimBackground = document.querySelector("#cover");
 const hamburger = document.querySelector(".hamburger");
 const closeIcon = document.querySelector(".closeIcon");
 const menuIcon = document.querySelector(".menuIcon");
@@ -8,10 +8,13 @@ const menuIcon = document.querySelector(".menuIcon");
 function toggleMenu() {
   if (nav.classList.contains("show-mobile-nav")) {
     nav.classList.remove("show-mobile-nav");
+    dimBackground.classList.remove("covering");
+
     closeIcon.style.display = "none";
     menuIcon.style.display = "block";
   } else {
     nav.classList.add("show-mobile-nav");
+    dimBackground.classList.add("covering");
     closeIcon.style.display = "block";
     menuIcon.style.display = "none";
   }
